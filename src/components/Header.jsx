4446c5eaa83fb2fe427ea97a8c49131fc6e133f2
adritorafsan <adritorafsan@gmail.com>
2024-03-22 22:21:57 +0600

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+// Import You Profile Img
+import logo from '../img/profile.jpg'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    const name = "Adrito Rafsan";
+    return (
+        <div className='my-36 ml-10 md:ml-[400px] max-w-screen-2xl'>
+            <div>
+                <div className='flex gap-5 items-center'><img className='rounded-full w-[60px] h-[60px]' src={logo} alt="" /><h1 className='font-poppins text-base font-bold  text-black/70'>{name}</h1> </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                   <div></div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       fontFamily:{
         poppins:['Poppins','sans-serif'],
+      },
+      colors:{
+        bgcolor: '#fcfcfb'
       }
     },
   },
