6565455e2cbc59df58e7149a94de5783a57d5dd3
adritorafsan <adritorafsan@gmail.com>
2024-03-23 21:43:54 +0600

    yo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ function App() {
 
   return (
     <>
+    <section  className='my-36 px-4 mx-10 md:mx-[400px] max-w-screen-2xl'>
+
     <Header />
+    </section>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,35 @@
+import { useState ,useEffect } from 'react';
 // Import You Profile Img
 import logo from '../img/profile.jpg'
 
+// Icons For Light and Dark Mode 
+import { FaMoon } from "react-icons/fa6";
+import { BiSun } from "react-icons/bi";
+
 const Header = () => {
+    const [Theme , setTheme] = useState(localStorage.getItem("Theme") ? localStorage.getItem("Theme") : "light");
+
+    useEffect(() => {
+     if (Theme === 'dark') {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem("Theme","dark");
+     }
+     else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem("Theme","light");
+
+     }
+
+    }, [Theme])
+    
+
     const name = "Adrito Rafsan";
     return (
-        <div className='my-36 ml-10 md:ml-[400px] max-w-screen-2xl'>
+        <div className=' max-w-screen-2xl'>
             <div>
-                <div className='flex gap-5 items-center'><img className='rounded-full w-[60px] h-[60px]' src={logo} alt="" /><h1 className='font-poppins text-base font-bold  text-black/70'>{name}</h1> </div>
+                <div className='flex justify-between items-center'><div className='flex gap-5 items-center'><img className='rounded-full cursor-pointer w-[60px] h-[60px]' src={logo} alt="" /><h1 className='font-poppins text-base font-bold dark:text-white text-black/70'>{name}</h1></div>{
+                    Theme === "dark" ? (< BiSun className='cursor-pointer text-3xl text-white' onClick={()=> setTheme('light')} />) : (< FaMoon className='cursor-pointer text-3xl' onClick={()=> setTheme('dark')} />)
+                }</div>
                 <div>
                     <div></div>
                     <div></div>
